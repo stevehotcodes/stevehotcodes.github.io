f620302b77b7433a519da3b45c8374c0b39d8ae6
stevehotcodes <stevehotcodes@gmail.com>
2023-04-22 16:20:52 +0300

    updates my project

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 async  function fetchdata(){
- await fetch('https://jsonplaceholder.typicode.com/users/4')
+ await fetch('https://jsonplaceholder.typicode.com/users/5')
  .then(res=>res.json())
  .then(data=>{
         let profiledata=data.name;
-        document.getElementById("u").innerHTML=profiledata
-        console.log(data);
+        let names=document.querySelectorAll(".name");
+        names.forEach(element => {
+            element.innerText=profiledata;
+            console.log(element);
+        });
+      
 
-      let username =data.name
-     document.getElementById("name").innerHTML=username;
+   //    // let username =data.name
+   //   document.getElementById("name").innerHTML=username;
      
-      console.log(data.name);
+   //    console.log(data.name);
 
     //  <div class="user-name">
     // <h2>${data.name}</h2>
@@ -22,12 +26,24 @@ async  function fetchdata(){
 fetchdata();
 
 async function getTweets(){
-   await fetch('https://jsonplaceholder.typicode.com/posts/3')
+   await fetch('https://jsonplaceholder.typicode.com/posts/6')
    .then(res=>res.json())
    .then(posts=>{
       let poster=posts.title;
-      document.getElementById("title1").innerHTML=poster;
-      console.log(poster)
+     let title1= document.querySelectorAll(".title1");
+     title1.forEach(element=>{
+      element.innerHTML=poster;
+     })
+      // console.log(poster)
+
+      let poster1=posts.body;
+     let body= document.querySelectorAll(".body");
+     body.forEach(element=>{
+      element.innerHTML=poster1;
+     })
+      
+
+
       let postBody=posts.body
       document.getElementById("body").innerHTML=postBody
    })
